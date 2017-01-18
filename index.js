a0f93bde95a26ab46886be5bafe9464818c6f5bf
Aluan Haddad <aluanh@gmail.com>
2017-01-17 21:34:18 -0500

    Use exports.default without cjs -> esm translation

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
 	this.value = content;
-	return "module.exports = " + JSON.stringify(content);
+	return "exports.default = " + JSON.stringify(content);
 }
